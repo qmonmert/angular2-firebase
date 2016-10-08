a768b1b070a87854c34cb9ea1a644c03f8d09d09
Quentin Monmert <q.monmert@proxiad.com>
2016-10-08 12:49:59 +0200

    Initialize firebase in a component with the npm resources

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initializeApp, database } from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  constructor() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyA2n9YUORKUMW2P-2vkhrc6R6X-NkGmaZY",
+      authDomain: "popping-fire-9851.firebaseapp.com",
+      databaseURL: "https://popping-fire-9851.firebaseio.com",
+      storageBucket: "popping-fire-9851.appspot.com",
+      messagingSenderId: "923784914853"
+    };
+    initializeApp(config);
+
+    var root = database().ref('trainings');
+
+    root.on('value', function(snap) {
+
+      console.log(snap.key, snap.val());
+
+    });
+  }
+
 }
